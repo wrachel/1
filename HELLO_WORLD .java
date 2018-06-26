4aa105da1f5722842c20fb3829411cd5639fb448
wrachel <Rwei951@gmail.com>
2018-06-26 10:18:01 -0700

    first commit
    First commit saying hello to the world

---
+++ HELLO_WORLD .java
@@ -0,0 +1,7 @@
+public class HELLO_WORLD {
+	public static void main(String[] args){
+		System.out.println("Hello, world!"); 
+	}
+}
+
+ 
---
+++ HELLO_WORLD.java
@@ -0,0 +1,9 @@
+public class HELLO_WORLD {
+	public static void main(String[] args){	
+		System.out.print("Hello, world!");
+		//printing "Hello, world!"
+		System.out.println("Hello, world!");
+	}
+}
+
+ 
