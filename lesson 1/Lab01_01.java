bc2e84510fbf2fe6103e037bc668953da37fa424
wrachel <Rwei951@gmail.com>
2018-06-27 21:51:06 -0700

    Labs 01_01 to 01_03, first challenge
    Includes Lab01_01, lab 01_02, lab 01_03, and challenge01_01

---
+++ lesson 1/Lab01_01.java
@@ -0,0 +1,15 @@
+public class Lab01_01{
+	public static void main(String[] args){	
+		System.out.print("Hello, world!");
+		//printing "Hello, world!"
+		System.out.println("Hello, world!");
+		//testing \n command
+		System.out.println("time for tea\ntea time?");
+		System.out.println("What's your name?");
+		//testing \t command 
+		System.out.println("Hello there.\tHello");
+		System.out.println("She said\tI've been reborn");
+	}
+}
+
+ 
---
+++ lesson 1/Lab01_03.java
@@ -0,0 +1,13 @@
+//Rachel Wei 6/27
+public class Lab01_03 {
+	public static void main (String[] args){
+		//making a :)ace
+		System.out.println("o       o\n    _");
+		//making a circle
+		System.out.println("0");
+		//making a square
+		System.out.println(" ______");
+		System.out.println("1          1");
+		System.out.println("1_____1");
+	}
+}
